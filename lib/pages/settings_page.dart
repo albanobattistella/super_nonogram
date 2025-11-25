2e721bc29a48e4568c42c7f1dfd0a69dd2a6b174
Adil Hanney <adilhanney@disroot.org>
2025-11-25 14:14:10 +0000

    chore: remove randomized colors on settings page

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -15,64 +15,59 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final colorScheme = theme.colorScheme;
     final iconTheme = IconTheme.of(context);
     final textTheme = theme.textTheme;
 
     return Scaffold(
       appBar: AppBar(title: Text(t.settings.settings)),
-      body: DefaultTextStyle.merge(
-        style: TextStyle(color: colorScheme.onSurface),
-        child: Center(
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 600),
-            child: ListView(
-              children: [
-                // hyperlegible font
-                SettingsItem(
-                  onTap: () => setState(() {
-                    stows.hyperlegibleFont.value =
-                        !stows.hyperlegibleFont.value;
-                  }),
-                  children: [
-                    Text(
-                      'Aa',
-                      style: textTheme.displaySmall?.copyWith(
-                        fontSize: (iconTheme.size ?? 24) * 0.8,
-                      ),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 600),
+          child: ListView(
+            children: [
+              // hyperlegible font
+              SettingsItem(
+                onTap: () => setState(() {
+                  stows.hyperlegibleFont.value = !stows.hyperlegibleFont.value;
+                }),
+                children: [
+                  Text(
+                    'Aa',
+                    style: textTheme.displaySmall?.copyWith(
+                      fontSize: (iconTheme.size ?? 24) * 0.8,
                     ),
-                    Text(t.settings.hyperlegibleFont),
-                    Switch.adaptive(
-                      value: stows.hyperlegibleFont.value,
-                      onChanged: (_) => setState(() {
-                        stows.hyperlegibleFont.value =
-                            !stows.hyperlegibleFont.value;
-                      }),
-                    ),
-                  ],
-                ),
-                const Divider(),
+                  ),
+                  Text(t.settings.hyperlegibleFont),
+                  Switch.adaptive(
+                    value: stows.hyperlegibleFont.value,
+                    onChanged: (_) => setState(() {
+                      stows.hyperlegibleFont.value =
+                          !stows.hyperlegibleFont.value;
+                    }),
+                  ),
+                ],
+              ),
+              const Divider(),
 
-                SettingsItem(
-                  onTap: () {
-                    showAboutDialog(
-                      context: context,
-                      applicationName: t.title.appName,
-                      applicationLegalese: t.settings.legalese,
-                    );
-                  },
-                  children: [
-                    const Icon(Icons.info),
-                    Text(t.title.appName),
-                    Text(t.settings.about, style: textTheme.labelMedium),
-                  ],
-                ),
-                const Divider(),
+              SettingsItem(
+                onTap: () {
+                  showAboutDialog(
+                    context: context,
+                    applicationName: t.title.appName,
+                    applicationLegalese: t.settings.legalese,
+                  );
+                },
+                children: [
+                  const Icon(Icons.info),
+                  Text(t.title.appName),
+                  Text(t.settings.about, style: textTheme.labelMedium),
+                ],
+              ),
+              const Divider(),
 
-                // something to put at the bottom after the last divider
-                const AnimatedAppIcon(),
-              ],
-            ),
+              // something to put at the bottom after the last divider
+              const AnimatedAppIcon(),
+            ],
           ),
         ),
       ),
